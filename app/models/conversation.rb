0d922e02ff6bc20e8009e19913e4d382e1768e74
Nikhil Kanthi <nkanthi@chicagobooth.edu>
2025-05-26 22:15:56 +0000

    Added spending habit inputs to conversations index page.

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -9,7 +9,7 @@
 #
 class Conversation < ApplicationRecord
   belongs_to :user, required: true, class_name: "User", foreign_key: "user_id"
-  has_one :spending_habit_input,
+  has_many :spending_habit_inputs,
           class_name:  "SpendingHabitInput",
           foreign_key: "conversation_id",
           dependent:   :destroy

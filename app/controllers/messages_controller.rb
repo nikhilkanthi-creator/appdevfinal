38648c513da6975a711acd258102229c2167c96f
Nikhil Kanthi <nkanthi@chicagobooth.edu>
2025-05-27 04:27:23 +0000

    All spending variables added to the prompt

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -28,10 +28,12 @@ class MessagesController < ApplicationController
     if the_message.valid?
       the_message.save
 
-      # 1. Get the message history
+      # 1. Get the message history and spending history
+
+      associated_spendinghabits = the_message.initial_spending_habits_input
 
       existing_messages = the_message.conversation.messages.order(:created_at)
-
+      
       # 2. Break this whole list of messages into system, user, and assistant arrays
 
       message_list = []
@@ -47,8 +49,7 @@ class MessagesController < ApplicationController
 
       # 2.1 add the spending habits to the user message
 
-      associated_spendinghabits = the_message.initial_spending_habits_input
-      message_list.push({ "role" => "user", "content" => "I spend #{associated_spendinghabits.groceries} on groceries."})
+     message_list.push({ "role" => "user", "content" => "I spend #{associated_spendinghabits.groceries} on groceries, #{associated_spendinghabits.gasfuel} on gas/fuel, #{associated_spendinghabits.diningrestaurants} on restaurants, #{associated_spendinghabits.travel_flights_hotels_uber} on travel. I have a loyalty towards #{associated_spendinghabits.airline_loyalty} airlines. My annual fee tolerance is #{associated_spendinghabits.annual_fee_tolerance}."})
 
 
       # 3. Now send the package to OpenAI:
@@ -74,21 +75,6 @@ class MessagesController < ApplicationController
       redirect_to("/conversations/#{the_message.conversation_id}", { :notice => "Message created successfully." })
     else
       redirect_to("/", { :alert => the_message.errors.full_messages.to_sentence })
-    end
-  end
-
-  def update
-    the_id = params.fetch("path_id")
-    the_message = Message.where({ :id => the_id }).at(0)
-
-    the_message.initial_spending_habits_input_id = params.fetch("query_initial_spending_habits_input_id")
-    the_message.user_id = params.fetch("query_user_id")
-
-    if the_message.valid?
-      the_message.save
-      redirect_to("/messages/#{the_message.id}", { :notice => "Message updated successfully."} )
-    else
-      redirect_to("/messages/#{the_message.id}", { :alert => the_message.errors.full_messages.to_sentence })
     end
   end
 

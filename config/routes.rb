929c1f2d5c2f1919789f6f1fb9f1bd23d40d06e3
Nikhil Kanthi <nkanthi@chicagobooth.edu>
2025-05-31 00:14:03 +0000

    trimmed down routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   # Routes for the Conversation resource:
 
   get("/", { :controller => "conversations", :action => "index"})
+  
   # CREATE
+
   post("/insert_conversation", { :controller => "conversations", :action => "create" })
           
   # READ
-  get("/conversations", { :controller => "conversations", :action => "index" })
   
   get("/conversations/:path_id", { :controller => "conversations", :action => "show" })
   
@@ -23,16 +24,7 @@ Rails.application.routes.draw do
 
   # CREATE
   post("/insert_message", { :controller => "messages", :action => "create" })
-          
-  # READ
-  get("/messages", { :controller => "messages", :action => "index" })
-  
-  get("/messages/:path_id", { :controller => "messages", :action => "show" })
-  
-  # UPDATE
-  
-  post("/modify_message/:path_id", { :controller => "messages", :action => "update" })
-  
+            
   # DELETE
   get("/delete_message/:path_id", { :controller => "messages", :action => "destroy" })
 
@@ -42,15 +34,6 @@ Rails.application.routes.draw do
 
   # CREATE
   post("/insert_spending_habit_input", { :controller => "spending_habit_inputs", :action => "create" })
-          
-  # READ
-  get("/spending_habit_inputs", { :controller => "spending_habit_inputs", :action => "index" })
-  
-  get("/spending_habit_inputs/:path_id", { :controller => "spending_habit_inputs", :action => "show" })
-  
-  # UPDATE
-  
-  post("/modify_spending_habit_input/:path_id", { :controller => "spending_habit_inputs", :action => "update" })
   
   # DELETE
   get("/delete_spending_habit_input/:path_id", { :controller => "spending_habit_inputs", :action => "destroy" })
